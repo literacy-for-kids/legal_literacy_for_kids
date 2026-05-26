1b1c2fa68b46a8fd19903301e7e4f357dade8264
Zac Cohen <zcohenld@gmail.com>
2026-05-26 07:44:37 -0400

    Add homepage hero image

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -1,11 +1,14 @@
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import Layout from '@theme/Layout';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import styles from './index.module.css';
 
 function HomepageHeader() {
+  const heroImageUrl = useBaseUrl('/img/hero-image.png');
+
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className={clsx('container', styles.heroContent)}>
@@ -34,6 +37,15 @@ function HomepageHeader() {
               View on GitHub
             </Link>
           </div>
+        </div>
+        <div className={styles.heroMedia}>
+          <img
+            className={styles.heroImage}
+            src={heroImageUrl}
+            alt="Illustration of children learning about rights, rules, fairness, and justice."
+            width="1536"
+            height="1024"
+          />
         </div>
       </div>
     </header>
